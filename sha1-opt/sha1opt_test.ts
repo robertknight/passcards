80faaeefdbac77df5898f4b17821b49a298b7889
Robert Knight <robertknight@gmail.com>
2014-04-19 18:01:19 +0100

    Convert sha1opt to TypeScript
    Additionally convert the test to import QUnit as a module
    rather than rely on globals exported by it.

--- sha1-opt/sha1opt_test.ts
+++ sha1-opt/sha1opt_test.ts
@@ -1,26 +1,34 @@
-var rusha_sha1 = require('rusha');
+/// <reference path="../typings/DefinitelyTyped/node/node.d.ts" />
+
+var qunit = require('qunitjs');
 var fastSha1 = require('./sha1opt');
 
-const testVectors = [
+var testVectors = [
 	{ msg : "abc",
 	  digest : "a9993e364706816aba3e25717850c26c9cd0d89d" },
 	{ msg : "abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq",
 	  digest : "84983e441c3bd26ebaae4aa1f95129e5e54670f1" }
 ]
 
-var testQueue = [];
-if (typeof test == 'undefined') {
-	test = function(name, func) {
-		testQueue.push(func);
-	}
-	equal = function(actual, expected, msg) {
-		if (actual != expected) {
-			console.log('Failed: ' + msg);
+qunit.done((result: any) => {
+	console.log('tests run. total: ' + result.total + ' failed: ' + result.failed);
+	if (typeof process != 'undefined') {
+		if (result.failed > 0) {
+			process.exit(1)
+		} else {
+			process.exit(0);
 		}
 	}
-}
+});
 
-test('SHA-1', function() {
+qunit.log((details: any) => {
+	if (!details.result) {
+		console.log('test failed');
+		console.log(details);
+	}
+});
+
+qunit.test('SHA-1', (assert: any) => {
 	var hash = new fastSha1.FastSha1();
 	testVectors.forEach(function(tst) {
 		var srcBuf = new Uint8Array(tst.msg.length);
@@ -28,15 +36,16 @@ test('SHA-1', function() {
 		var digest = new Int32Array(5);
 		hash.sha1(srcBuf, srcBuf.length, digest);
 		var actual = fastSha1.hexlify(digest);
-		equal(actual, tst.digest, 'check SHA-1 digests match');
+		assert.equal(actual, tst.digest, 'check SHA-1 digests match');
 	});
 });
 
-testQueue.forEach(function(tst) {
-	tst();
-});
+qunit.test('SHA-1 benchmark', (assert: any) => {
+	qunit.expect(0);
 
-window.testSha1 = function() {
+	// SHA-1 benchmark that mirrors usage by 1Password 4's
+	// current default settings of 80K PBKDF2 iterations
+	// using HMAC-SHA1
 	var ITER = 160000;
 	var hash = new fastSha1.FastSha1();
 	var srcBuf = new Uint8Array(500);
@@ -56,5 +65,11 @@ window.testSha1 = function() {
 	}
 	var end = new Date().getTime();
 	return end-start;
+
+	assert.equal(fastSha1.hexlify(digestBuf), 'dd79feb54a1097f8831511df9dd4522cf61063e8');
+});
+
+if (typeof window == 'undefined') {
+	qunit.load();
 }
 
