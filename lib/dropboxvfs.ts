e4b54632297a73dd5be8f0588c8454bfb0c15636
Robert Knight <robertknight@gmail.com>
2014-04-21 15:27:19 +0100

    Improve typings for Dropbox file stat info objects and callbacks

--- lib/dropboxvfs.ts
+++ lib/dropboxvfs.ts
@@ -6,13 +6,13 @@ import fs = require('fs');
 import vfs = require('./vfs');
 
 export class DropboxVFS implements vfs.VFS {
-	private client : any;
+	private client : dropbox.Client;
 
 	constructor() {
 		var apiKeys : any = JSON.parse(fs.readFileSync('dropbox-key.json').toString());
 		this.client = new dropbox.Client(apiKeys);
 		this.client.authDriver(new dropbox.AuthDriver.NodeServer(8191));
-		this.client.onError.addListener(function(error: any) {
+		this.client.onError.addListener((error) => {
 			console.log(error);
 		});
 	}
@@ -28,9 +28,9 @@ export class DropboxVFS implements vfs.VFS {
 
 	/** Search for files whose name contains @p namePattern */
 	search(namePattern: string, cb: (files: vfs.FileInfo[]) => any) {
-		this.client.search('/', namePattern, {}, (err: any, files: any[]) => {
+		this.client.search('/', namePattern, {}, (err, files) => {
 			var fileList : vfs.FileInfo[] = [];
-			files.forEach((file:any) => {
+			files.forEach((file) => {
 				fileList.push(this.toVfsFile(file));
 			});
 			cb(fileList);
@@ -39,21 +39,21 @@ export class DropboxVFS implements vfs.VFS {
 
 	/** Read the contents of a file at @p path */
 	read(path: string, cb: (error: any, content:string) => any) {
-		this.client.readFile(path, {}, (error: any, content: string) => {
+		this.client.readFile(path, {}, (error, content) => {
 			cb(error, content);
 		});
 	}
 
 	/** Write the contents of a file at @p path */
 	write(path: string, content: string, cb: (error:any) => any) {
-		this.client.writeFile(path, content, {}, (error:any) => {
+		this.client.writeFile(path, content, {}, (error) => {
 			cb(error);
 		});
 	}
 
 	/** List the contents of a directory */
 	list(path: string, cb: (error: any, files: vfs.FileInfo[]) => any) {
-		this.client.readdir(path, {}, (error:any, names : string[], folderInfo:any, files:any[]) => {
+		this.client.readdir(path, {}, (error, names, folderInfo, files) => {
 			if (error) {
 				cb(error, []);
 				return;
@@ -68,7 +68,7 @@ export class DropboxVFS implements vfs.VFS {
 
 	/** Remove a file */
 	rm(path: string, cb: (error: any) => any) {
-		this.client.remove(path, (error: any) => {
+		this.client.remove(path, (error) => {
 			cb(error);
 		});
 	}
--- typings/dropbox.d.ts
+++ typings/dropbox.d.ts
@@ -34,9 +34,33 @@ declare module "dropbox" {
 		// TODO
 	}
 
+	export interface SearchOptions {
+		// TODO
+	}
+
 	export module AuthDriver {
 		export var NodeServer : {
 			new(port: number) : AuthDriver;
+		}
+	}
+
+	export module File {
+		export interface Stat {
+			name: string;
+			path: string;
+			inAppFolder: boolean;
+			isFolder: boolean;
+			isFile: boolean;
+			isRemoved: boolean;
+			typeIcon: string;
+			versionTag: string;
+			contentHash: string;
+			mimeType: string;
+			size: number;
+			humanSize: string;
+			hasThumbnail: boolean;
+			modifiedAt: Date;
+			clientModifiedAt: Date;
 		}
 	}
 
@@ -46,13 +70,15 @@ declare module "dropbox" {
 
 	export interface Client {
 		authDriver(driver: AuthDriver) : Client;
-		authenticate(callback : (error: any, account: string) => any) : Client;
+		authenticate(callback : (error: ApiError, account: string) => any) : Client;
 		isAuthenticated() : boolean;
-		readFile(path: string, options : ReadFileOptions, callback: (error: any, content: string) => void) : XMLHttpRequest;
-		writeFile(path: string, content: string, options : WriteFileOptions, callback: (error:any) => void) : XMLHttpRequest;
-		readdir(path: string, options : ReadDirOptions, callback: (error: any, names: string[], folderInfo: any, files: any[]) => void) : XMLHttpRequest;
-		remove(path: string, callback: (error: any) => void) : XMLHttpRequest;
-		setCredentials(credentials: any) : Client;
+		readFile(path: string, options : ReadFileOptions, callback: (error: ApiError, content: string) => void) : XMLHttpRequest;
+		writeFile(path: string, content: string, options : WriteFileOptions, callback: (error:ApiError) => void) : XMLHttpRequest;
+		readdir(path: string, options : ReadDirOptions, callback: (error: any, names: string[], folderInfo: File.Stat, files: File.Stat[]) => void) : XMLHttpRequest;
+		remove(path: string, callback: (error: ApiError) => void) : XMLHttpRequest;
+		search(path: string, namePattern: string, options: SearchOptions, callback: (error:ApiError, matches: File.Stat[]) => void) : XMLHttpRequest;
+		setCredentials(credentials: Object) : Client;
+		credentials() : Object;
 		onError : EventSource<ApiError>;
 	}
 }
