988129fca755864f5e49cabf8d41f728e19beb91
Robert Knight <robert.knight@mendeley.com>
2014-05-09 21:23:54 +0100

    Redirect to an https:// location when accessing the app from a non-localhost host
    For security the OAuth login flow requires the redirect URI to be an
    HTTPS endpoint. We also want to ensure that the user is receiving the
    correct copy of the app.

--- webui/init.ts
+++ webui/init.ts
@@ -2,6 +2,14 @@ import $ = require('jquery');
 import app = require('./app');
 
 $(document).ready(() => {
+	// redirect to a secure connection unless this copy of the app
+	// is being hosted locally
+	var location = document.location.href;
+	if (location.indexOf('http:') == 0 && location.indexOf('http://localhost') == -1) {
+		document.location.href = location.replace('http:', 'https:');
+		return;
+	}
+
 	new app.App();
 });
 
