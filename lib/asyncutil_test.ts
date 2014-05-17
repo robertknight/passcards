fc192d4c0baa70da3e523f069d2332ee3a3278b5
Robert Knight <robertknight@gmail.com>
2014-05-17 16:27:31 +0100

    Add a missing unit test suite file to Git

---
+++ lib/asyncutil_test.ts
@@ -0,0 +1,26 @@
+import Q = require('q');
+
+import testLib = require('./test');
+import asyncutil = require('./asyncutil');
+
+testLib.addAsyncTest('test run sequence', (assert) => {
+	var values = [1, 1, 2, 3, 5, 8, 13];
+	var runOrder : number[] = [];
+
+	var funcs : Array<() => Q.Promise<number>> = [];
+	values.forEach((value, index) => {
+		funcs.push(() => {
+			runOrder.push(index+1);
+			return Q.resolve(value);
+		});
+	});
+
+	asyncutil.runSequence(funcs).then((result) => {
+		testLib.assertEqual(assert, result, values);
+		testLib.assertEqual(assert, runOrder, [1, 2, 3, 4, 5, 6, 7]);
+		testLib.continueTests();
+	});
+});
+
+testLib.runTests();
+
