5d6292071503e6d210ac37e7f4dd98102dc7ce1e
Robert Knight <robertknight@gmail.com>
2014-05-17 11:07:46 +0100

    WIP - Add react dependency and stubs for UI views

--- webui/app.ts
+++ webui/app.ts
@@ -1,11 +1,69 @@
 /// <reference path="../typings/DefinitelyTyped/jquery/jquery.d.ts" />
 /// <reference path="../typings/DefinitelyTyped/q/Q.d.ts" />
+/// <reference path="../node_modules/react-typescript/declarations/react.d.ts" />
+/// <reference path="../node_modules/react-typescript/declarations/react-typescript.d.ts" />
 
 import $ = require('jquery');
 import Q = require('q');
+import react = require('react');
+import reactts = require('react-typescript');
 
 import dropboxvfs = require('../lib/dropboxvfs');
 import onepass = require('../lib/onepass');
+
+class AppView extends reactts.ReactComponentBase<{}, {}> {
+	render() {
+		return new ItemList({
+			className: 'commentBox',
+			children: 'Hello - I am a react element'
+		});
+	}
+}
+
+// View for entering master password and unlocking the vault
+class UnlockPane extends reactts.ReactComponentBase<{}, {}> {
+	render() {
+		return react.DOM.div({
+			children: 'This is the vault unlock pane'
+		});
+	}
+}
+
+// Search box to search through items in the view
+class SearchField extends reactts.ReactComponentBase<{}, {}> {
+	render() {
+		return react.DOM.div({
+			children: 'This is a search field for the item view'
+		});
+	}
+}
+
+// Detail view for an individual item
+class DetailsView extends reactts.ReactComponentBase<{}, {}> {
+	render() {
+		return react.DOM.div({
+			children: 'This is the details view for an item'
+		});
+	}
+}
+
+// Item in the overall view
+class Item extends reactts.ReactComponentBase<{}, {}> {
+	render() {
+		return react.DOM.div({
+			children: 'This is an item'
+		});
+	}
+}
+
+// List of all items in the vault
+class ItemList extends reactts.ReactComponentBase<{}, {}> {
+	render() {
+		return react.DOM.div({
+			children: 'This is the item list'
+		});
+	}
+}
 
 export class App {
 	vault : Q.Promise<onepass.Vault>;
@@ -57,6 +115,8 @@ export class App {
 				lockStatus.text('Unlocking failed: ' + err);
 			});
 		});
+
+		react.renderComponent(new AppView({}), document.getElementById('react-content'));
 	}
 
 	showDetails(item: onepass.Item) {
