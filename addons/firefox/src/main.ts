ecc74ba4e3ba5b1dab63f200a354e60aa56d35e8
Robert Knight <robertknight@gmail.com>
2014-06-30 19:09:01 +0100

    Use TypeScript for Firefox add-on stub
     * Add src/ dir for .ts sources, use lib/ and data/
       for the built resources
    
     * Add stub file for Firefox add-on SDK typings

---
+++ addons/firefox/src/main.ts
@@ -0,0 +1,42 @@
+/// <reference path="../typings/firefox-addon-sdk.d.ts" />
+
+var buttons = require('sdk/ui/button/toggle');
+var panel = require('sdk/panel');
+var tabs = require('sdk/tabs');
+
+// use `self_` to avoid conflict with `self`
+// in lib.d.ts
+var self_ = require('sdk/self');
+
+var mainPanel;
+
+var showPanel = (state) => {
+	mainPanel = panel.Panel({
+		width: 300,
+		height: 400,
+		contentScriptFile : self_.data.url('webui_bundle.js'),
+		contentURL : self_.data.url('panel.html'),
+		onHide: onPanelHidden
+	});
+
+	if (state.checked) {
+		mainPanel.show({
+			position: toolbarButton
+		});
+	}
+};
+
+var toolbarButton = buttons.ToggleButton({
+	id: 'key-icon',
+	label: 'Password Manager',
+	icon: {
+		'16' : './icon-16.png',
+		'32' : './icon-32.png',
+		'64' : './icon-64.png'
+	},
+	onChange: showPanel
+});
+
+function onPanelHidden() {
+	toolbarButton.state('window', { checked: false });
+};
