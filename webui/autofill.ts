02b7a74a40702066aa16956b7e55432196e36a07
Robert Knight <robertknight@gmail.com>
2014-07-07 22:09:24 +0100

    Adapt page_access import name to match variable name rules

--- webui/autofill.ts
+++ webui/autofill.ts
@@ -1,7 +1,7 @@
 import Q = require('q');
 
 import onepass = require('../lib/onepass');
-import page_access = require('./page_access');
+import pageAccess = require('./page_access');
 import stringutil = require('../lib/base/stringutil');
 
 /** Interface for initiating auto-fill of items in
@@ -11,45 +11,45 @@ export interface AutoFillHandler {
 	/** Autofill fields on the current page with values from a given @p item.
 	  * Returns a promise for the auto-filled entries.
 	  */
-	autofill(item: onepass.Item) : Q.Promise<page_access.AutoFillEntry[]>;
+	autofill(item: onepass.Item) : Q.Promise<pageAccess.AutoFillEntry[]>;
 }
 
 export class AutoFiller {
-	private pageAccess: page_access.PageAccess;
+	private page: pageAccess.PageAccess;
 
-	constructor(pageAccess: page_access.PageAccess) {
-		this.pageAccess = pageAccess;
+	constructor(page: pageAccess.PageAccess) {
+		this.page = page;
 	}
 
 	// match the ID or name of a field against a key
-	private fieldMatch(field: page_access.InputField, key: string) {
+	private fieldMatch(field: pageAccess.InputField, key: string) {
 		var keyMatch = (propertyValue: string) => {
 			return propertyValue && stringutil.indexOfIgnoreCase(propertyValue, key) != -1;
 		}
 		return keyMatch(field.id) || keyMatch(field.name) || keyMatch(field.ariaLabel) || keyMatch(field.placeholder);
 	}
 
-	autofill(item: onepass.Item) : Q.Promise<page_access.AutoFillEntry[]> {
-		var result = Q.defer<page_access.AutoFillEntry[]>();
+	autofill(item: onepass.Item) : Q.Promise<pageAccess.AutoFillEntry[]> {
+		var result = Q.defer<pageAccess.AutoFillEntry[]>();
 		var usernameKeys = ['email', 'user', 'account'];
 
 		item.getContent().then((content) => {
-			this.pageAccess.findForms((fields) => {
-				var autofillEntries: page_access.AutoFillEntry[] = [];
+			this.page.findForms((fields) => {
+				var autofillEntries: pageAccess.AutoFillEntry[] = [];
 
 				fields.forEach((field) => {
 					var isUsernameField = false;
 					var isPasswordField = false;
 
 					if (this.fieldMatch(field, 'password') ||
-					    field.type == page_access.FieldType.Password) {
+					    field.type == pageAccess.FieldType.Password) {
 						isPasswordField = true;
 					}
 
 					if (!isPasswordField) {
 						usernameKeys.forEach((key) => {
 							if (this.fieldMatch(field, key) ||
-								field.type === page_access.FieldType.Email) {
+								field.type === pageAccess.FieldType.Email) {
 								isUsernameField = true;
 							}
 						});
@@ -63,7 +63,7 @@ export class AutoFiller {
 					}
 
 					if (autofillValue) {
-						var entry: page_access.AutoFillEntry = {
+						var entry: pageAccess.AutoFillEntry = {
 							fieldId: field.id,
 							fieldName: field.name,
 							value: autofillValue
@@ -72,7 +72,7 @@ export class AutoFiller {
 					}
 				});
 
-				this.pageAccess.autofill(autofillEntries);
+				this.page.autofill(autofillEntries);
 				result.resolve(autofillEntries);
 			});
 		}).done();
