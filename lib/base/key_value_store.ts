4c0255526308106fc894a63cb29863e12ace64f8
Robert Knight <robertknight@gmail.com>
2014-08-11 22:18:08 +0200

    Add IndexedDB-based cache for site icons

---
+++ lib/base/key_value_store.ts
@@ -0,0 +1,94 @@
+/// <reference path="../../typings/DefinitelyTyped/q/Q.d.ts" />
+/// <reference path="../../typings/dom.d.ts" />
+
+import Q = require('q');
+
+import stringutil = require('./stringutil');
+
+/** Interface for async key/value storage. */
+export interface Store {
+	set<T>(key: string, value: T) : Q.Promise<void>;
+	get<T>(key: string) : Q.Promise<T>;
+	remove(key: string) : Q.Promise<void>;
+	list(prefix: string) : Q.Promise<string[]>;
+}
+
+/** Implementation of async key/value storage using IndexedDB */
+export class IndexedDBStore implements Store {
+	private db: Q.Promise<IDBDatabase>;
+
+	constructor(public dbName: string, public storeName: string) {
+		var DB_VERSION = 1;
+		var _db = Q.defer<IDBDatabase>();
+
+		var req = indexedDB.open(dbName, DB_VERSION);
+		req.onupgradeneeded = () => {
+			var db = <IDBDatabase>(req.result);
+			db.createObjectStore(this.storeName);
+		};
+		req.onsuccess = () => {
+			var db = <IDBDatabase>(req.result);
+			_db.resolve(db);
+		};
+		req.onerror = (err) => {
+			_db.reject(err);
+		};
+
+		this.db = _db.promise;
+	}
+
+	set<T>(key: string, value: T) : Q.Promise<void> {
+		return this.db.then((db) => {
+			var store = db.transaction(this.storeName, 'readwrite').objectStore(this.storeName);
+			return this.promisify<void>(store.put(value, key));
+		});
+	}
+
+	get<T>(key: string) : Q.Promise<T> {
+		return this.db.then((db) => {
+			var store = db.transaction(this.storeName, 'readonly').objectStore(this.storeName);
+			return this.promisify<T>(store.get(key));
+		});
+	}
+
+	remove(key: string) : Q.Promise<void> {
+		return this.db.then((db) => {
+			var store = db.transaction(this.storeName, 'readwrite').objectStore(this.storeName);
+			return this.promisify<void>(store.delete(key));
+		});
+	}
+
+	list(prefix: string) : Q.Promise<string[]> {
+		return this.db.then((db) => {
+			var store = db.transaction(this.storeName, 'readwrite').objectStore(this.storeName);
+			var req = store.openCursor(IDBKeyRange.lowerBound(prefix));
+			var keys: string[] = [];
+			var result = Q.defer<string[]>();
+
+			req.onsuccess = () => {
+				var cursor = <IDBCursor>req.result;
+				if (!stringutil.startsWith(cursor.key, prefix)) {
+					result.resolve(keys);
+				}
+				keys.push(cursor.key);
+			};
+			req.onerror = () => {
+				result.reject(req.error);
+			};
+
+			return result.promise;
+		});
+	}
+
+	private promisify<T>(req: IDBRequest) : Q.Promise<T> {
+		var result = Q.defer<T>();
+		req.onsuccess = () => {
+			result.resolve(req.result);
+		};
+		req.onerror = () => {
+			result.reject(req.error);
+		};
+		return result.promise;
+	}
+}
+
---
+++ typings/dom.d.ts
@@ -0,0 +1,23 @@
+// this file contains DOM declarations and corrections for
+// methods missing from TypeScript's lib.d.ts file
+
+// [TS/1.0] IDBKeyRange static methods are
+// represented as interface methods
+declare var IDBKeyRange : {
+	bound(lower: any, upper: any, lowerOpen?: boolean, upperOpen?: boolean): IDBKeyRange;
+    only(value: any): IDBKeyRange;
+    lowerBound(bound: any, open?: boolean): IDBKeyRange;
+    upperBound(bound: any, open?: boolean): IDBKeyRange;
+}
+
+/*
+interface BlobPropertyBag {
+	type?: string;
+	endings?: string;
+}
+
+interface Blob {
+	// [TS/1.0] - Blob ctor is missing args
+	new(parts?: Array, props?: BlobPropertyBag);
+}
+*/
