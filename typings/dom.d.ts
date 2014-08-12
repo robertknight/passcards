41940c3e7a577523f06b6723370439da5a1073b8
Robert Knight <robertknight@gmail.com>
2014-08-12 16:23:54 +0200

    Work around missing IDBKeyRange static interface in TS 1.0
    The static side of IDBKeyRange's interface is missing from TS 1.0's
    lib.d.ts. Import the current definition from TS 1.1's lib.dom.d.ts file.

--- typings/dom.d.ts
+++ typings/dom.d.ts
@@ -1,12 +1,12 @@
 // this file contains DOM declarations and corrections for
 // methods missing from TypeScript's lib.d.ts file
 
-// [TS/1.0] IDBKeyRange static methods are
-// represented as interface methods
-/*declare var IDBKeyRange : {
-	bound(lower: any, upper: any, lowerOpen?: boolean, upperOpen?: boolean): IDBKeyRange;
+// [TS/1.0] IDBKeyRange static interface is missing
+declare var IDBKeyRange: {
+    prototype: IDBKeyRange;
+    new(): IDBKeyRange;
+    bound(lower: any, upper: any, lowerOpen?: boolean, upperOpen?: boolean): IDBKeyRange;
     only(value: any): IDBKeyRange;
     lowerBound(bound: any, open?: boolean): IDBKeyRange;
     upperBound(bound: any, open?: boolean): IDBKeyRange;
-}*/
-
+}
