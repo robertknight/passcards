8964ebddc407d06524ca9bdab48c4b36c35b4dc4
Robert Knight <robertknight@gmail.com>
2014-08-20 08:57:58 +0100

    Standardize formatting of Chrome extension manifest

---
+++ utils/jsonfmt.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+var fs = require('fs');
+var INDENT = 2;
+
+for (var i=2; i < process.argv.length; i++) {
+	var jsonFilePath = process.argv[i];
+	var content = JSON.parse(fs.readFileSync(jsonFilePath));
+	fs.writeFileSync(jsonFilePath, JSON.stringify(content, null /* replacer */, INDENT));
+}
