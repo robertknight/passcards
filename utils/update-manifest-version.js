1e0dfa292c509d0e4503aff665c9d562881bda98
Robert Knight <robertknight@gmail.com>
2014-08-21 21:19:57 +0100

    Promote repository to a full clone before calculating number of commits in current branch
    In version number is determined based on the number of commits in
    the current branch so in order for it to be correct
    a full Git clone is required.

--- utils/update-manifest-version.js
+++ utils/update-manifest-version.js
@@ -1,5 +1,20 @@
 #!/usr/bin/env node
 
+// Update the 'version' field in a JSON manifest
+// file based on the number of commits since
+// the initial commit in the repository.
+//
+// This emulates SVN-like revision numbers in Git,
+// although this only works provided that
+// all commits are made from the same branch and
+// that the branch head only changes via addition
+// of new commits.
+//
+// This also requires full Git history in the local
+// repository. The script will convert the current
+// repository from a shallow to full clone if necessary
+// before upating the manifest.
+
 var child_process = require('child_process');
 var fs = require('fs');
 var Q = require('q');
@@ -11,16 +26,29 @@ function exec() {
 	proc.stdout.on('data', function(data) {
 		stdout += data.toString();
 	});
+	proc.stderr.on('data', function(data) {
+		console.log(data.toString());
+	});
 	proc.on('close', function(status) {
 		result.resolve([status, stdout]);
 	});
 	return result.promise;
 }
 
+function convertToFullRepo() {
+	if (fs.existsSync('.git/shallow')) {
+		return exec('git', 'fetch', '--unshallow');
+	} else {
+		return Q();
+	}
+}
+
 var manifestFile = process.argv[2];
 var key = process.argv[3] || 'version';
 
-exec('git', 'log', '--format="%h"').then(function(result) {
+convertToFullRepo().then(function() {
+	return exec('git', 'log', '--format="%h"')
+}).then(function(result) {
 	var status = result[0];
 	var commitList = result[1];
 
@@ -34,5 +62,5 @@ exec('git', 'log', '--format="%h"').then(function(result) {
 
 	manifest.version = newVersion;
 	fs.writeFileSync(manifestFile, JSON.stringify(manifest, null /* replacer */, 2) + '\n')
-});
+}).done();
 
