f85946674113f60de1febdebd4cd54980c583b6b
Robert Knight <robertknight@gmail.com>
2014-09-19 07:46:08 +0100

    Simplify and improve documentation of logic for forwarding messages from app UI to Firefox extension core
    Much of what panel_content.ts does is forwarding RPC messages
    from the addon's UI to the priviledged extension code which then
    uses priviledged APIs to perform an operation or forwards the request
    on to the active tab.
    
    Remove some duplication for these calls and add documentaton to clarify
    where each RPC handler receives its requests from or sends them to.

--- addons/firefox/src/panel_content.ts
+++ addons/firefox/src/panel_content.ts
@@ -22,53 +22,55 @@ if (stringutil.startsWith(window.location.href, OAUTH_REDIRECT_URL)) {
 }
 
 var pageAccess = createObjectIn<page_access.ExtensionConnector>(unsafeWindow, { defineAs: 'firefoxAddOn' });
-var addonRpc = new rpc.RpcHandler(selfWorker.port);
-var appRpc = new rpc.RpcHandler(new rpc.WindowMessagePort(document.defaultView, '*', 'extension-core', 'extension-app'));
-appRpc.clone = (data) => {
+
+// setup handler that sends/receives messages from the priviledged add-on
+// code (in src/main.ts)
+var extensionRpc = new rpc.RpcHandler(selfWorker.port);
+
+// setup handler that sends/receives messages from the unpriviledged UI
+// for passcards
+var appUiRpc = new rpc.RpcHandler(new rpc.WindowMessagePort(document.defaultView, '*', 'extension-core', 'extension-app'));
+appUiRpc.clone = (data) => {
 	return cloneInto(data, unsafeWindow);
 };
 
 pageAccess.syncService = selfWorker.options.syncService;
 pageAccess.oauthRedirectUrl = OAUTH_REDIRECT_URL;
 
-appRpc.onAsync('find-fields', (done) => {
-	addonRpc.call('find-fields', [], (err: any, fields: forms.InputField[]) => {
-		done(err, fields);
+// forwards an RPC request from the app UI to the priviledged extension code,
+// which in turn may forward the request to the active tab. When the main
+// extension script returns a reply, that is forwarded back to the app UI
+// via the `done` callback
+var forwardAsyncRpcCall = (method: string, done: Function, ...args: any[]) => {
+	extensionRpc.call(method, args, (err: any, ...result: any[]) => {
+		done.apply(null, [err].concat(result));
 	});
+}
+
+appUiRpc.onAsync('find-fields', forwardAsyncRpcCall.bind(null, 'find-fields'));
+appUiRpc.onAsync('autofill', forwardAsyncRpcCall.bind(null, 'autofill'));
+appUiRpc.onAsync('fetch-url', forwardAsyncRpcCall.bind(null, 'fetch-url'));
+
+appUiRpc.on('hide-panel', () => {
+	extensionRpc.call('hide-panel', []);
 });
 
-appRpc.onAsync('autofill', (done: (err: any, count: number) => void, fields: forms.AutoFillEntry[]) => {
-	addonRpc.call('autofill', [fields], (err: any, count: number) => {
-		done(err, count);
-	});
+appUiRpc.on<void>('copy', (mimeType: string, data: string) => {
+	extensionRpc.call('copy', [mimeType, data]);
 });
 
-appRpc.onAsync('fetch-url', (done: (err:any, info: site_info_service.UrlResponse) => void, url: string) => {
-	addonRpc.call('fetch-url', [url], (err: any, info: site_info_service.UrlResponse) => {
-		done(err, info);
-	});
-});
-
-appRpc.on('hide-panel', () => {
-	addonRpc.call('hide-panel', []);
-});
-
-appRpc.on<void>('copy', (mimeType: string, data: string) => {
-	addonRpc.call('copy', [mimeType, data]);
-});
-
-addonRpc.on<void>('pagechanged', (url: string) => {
+extensionRpc.on<void>('pagechanged', (url: string) => {
 	pageAccess.currentUrl = url;
-	appRpc.call('pagechanged', [url]);
+	appUiRpc.call('pagechanged', [url]);
 });
 
-addonRpc.on('show', () => {
-	appRpc.call('show', []);
+extensionRpc.on('show', () => {
+	appUiRpc.call('show', []);
 });
 
 // notify the add-on that the panel content script is ready.
 // Note that the panel content script is loaded _before_
 // the front-end so this event does not indicate that
 // the front-end itself is ready.
-addonRpc.call('ready', []);
+extensionRpc.call('ready', []);
 
