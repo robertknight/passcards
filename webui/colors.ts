ddacfe5457235ef48bf4e2d243adfc9db2dc0ebc
Robert Knight <robert.knight@mendeley.com>
2014-12-03 08:26:11 +0000

    Port unlock view styling to TypeScript

--- webui/colors.ts
+++ webui/colors.ts
@@ -1,5 +1,11 @@
+
+// see http://www.google.com/design/spec/style/typography.html
 var colors = {
-	MATERIAL_COLOR_PRIMARY: '#e91e63'
+	MATERIAL_COLOR_PRIMARY: '#e91e63',
+	MATERIAL_COLOR_HEADER: '#fff',
+	MATERIAL_COLOR_ACCENT1: '#fce4ec',
+	MATERIAL_COLOR_ACCENT2: '#f48fb1',
+	MATERIAL_COLOR_ACCENT3: '#f06292'
 };
 
 export = colors;
--- webui/unlock_view.ts
+++ webui/unlock_view.ts
@@ -5,9 +5,82 @@
 import react = require('react');
 import typed_react = require('typed-react');
 
+import colors = require('./colors');
 import focus_mixin = require('./base/focus_mixin');
 import item_store = require('../lib/item_store');
 import reactutil = require('./reactutil');
+import style = require('./base/style');
+
+var styles = style.create({
+	unlockPane: {
+		upper: {
+			backgroundColor: colors.MATERIAL_COLOR_PRIMARY,
+			position: 'absolute',
+			top: 0,
+			left: 0,
+			right: 0,
+			bottom: '60%',
+			boxShadow: 'rgba(0, 0, 0, 0.26) 0px 2px 5px 0px',
+			zIndex: '2'
+		},
+
+		lower: {
+			backgroundColor: colors.MATERIAL_COLOR_PRIMARY,
+			position: 'absolute',
+			left: 0,
+			top: '40%',
+			right: 0,
+			bottom: 0,
+			boxShadow: 'rgba(0, 0, 0, 0.26) 0px 2px -5px 0px',
+			zIndex: '1'
+		},
+
+		form: {
+			backgroundColor: colors.MATERIAL_COLOR_ACCENT3,
+			position: 'absolute',
+			left: 0,
+			right: 0,
+			bottom: 0,
+			height: '75%'
+		},
+
+		inputPane: {
+			display: 'flex',
+			flexDirection: 'column',
+			justifyContent: 'center',
+			width: '50%',
+			minWidth: 200,
+			maxWidth: 300,
+			marginLeft: 'auto',
+			marginRight: 'auto',
+			marginTop: '10%'
+		},
+
+		masterPasswordField: {
+			padding: 5,
+			border: '1px solid #fff',
+			borderRadius: 5,
+			fontSize: 18,
+			fontWeight: '400',
+			color: colors.MATERIAL_COLOR_HEADER,
+			backgroundColor: colors.MATERIAL_COLOR_ACCENT3,
+			outline: 'none'
+		},
+
+		'masterPasswordField::-webkit-input-placeholder': {
+			color: '#fff',
+			opacity: '0.8'
+		},
+
+		unlockLabel: {
+			width: '100%',
+			marginTop: 5,
+			color: 'white',
+			fontSize: 14,
+			fontWeight: 'bold'
+		}
+	}
+});
 
 enum UnlockState {
 	Locked,
@@ -56,10 +129,10 @@ export class UnlockView extends typed_react.Component<UnlockViewProps, UnlockVie
 		var unlockPaneLower: react.ReactElement<any,any>;
 
 		if (this.props.isLocked) {
-			unlockPaneUpper = react.DOM.div({className:'unlockPane upper'},
-				react.DOM.div({className:'unlockPaneForm'},
+			unlockPaneUpper = react.DOM.div({className: style.classes(styles.unlockPane.upper)},
+				react.DOM.div({className: style.classes(styles.unlockPane.form)},
 					react.DOM.form({
-						className: 'unlockPaneInputs',
+						className: style.classes(styles.unlockPane.inputPane),
 						ref:'unlockPaneForm',
 						onSubmit: (e) => {
 							e.preventDefault();
@@ -68,20 +141,20 @@ export class UnlockView extends typed_react.Component<UnlockViewProps, UnlockVie
 						}
 					},
 						react.DOM.input({
-							className: 'masterPassField',
+							className: style.classes(styles.unlockPane.masterPasswordField),
 							type: 'password',
 							placeholder: 'Master Password...',
 							ref: 'masterPassField',
 							autoFocus: true
 						}),
-						react.DOM.div({className: 'unlockLabel'}, unlockMessage)
+						react.DOM.div({className: style.classes(styles.unlockPane.unlockLabel)}, unlockMessage)
 					)
 				)
 			);
-			unlockPaneLower = react.DOM.div({className:'unlockPane lower'});
+			unlockPaneLower = react.DOM.div({className: style.classes(styles.unlockPane.lower)});
 		}
 
-		return react.DOM.div({className: 'unlockPane'},
+		return react.DOM.div({className: style.classes(styles.unlockPane)},
 			reactutil.CSSTransitionGroupF({
 				transitionName: 'slide-from-top'
 			}, unlockPaneUpper),
