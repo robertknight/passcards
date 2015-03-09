16e7ba30820d46de6d830d1e9c4bbbb52e0d7e13
Robert Knight <robert.knight@mendeley.com>
2015-03-09 09:19:36 +0000

    Work-around incorrect formatting for function call args
    TS 1.4 has a bug which removes the space between the comma
    and function call arguments that start with an open parens.
    
    This is fixed upstream in 929d359bdfb880339845cb88df0bb959a89a9220
    which should land in TS 1.5.

--- utils/format-source.js
+++ utils/format-source.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 var findit = require('findit');
+var fs = require('fs');
 var path = require('path');
 var Q = require('q');
 var typescript_formatter = require('typescript-formatter');
@@ -33,15 +34,36 @@ function findTypeScriptFiles(dir) {
 	return files.promise;
 }
 
+function fixupResult(source) {
+	// Fix up incorrect formatting of function call arguments
+	// that start with open parens:
+	//
+	//   'foo(a, b,() => {' => 'foo(a, b, () =>'
+	// 
+	// Fixed upstream in TypeScript by 929d359bdfb880339845cb88df0bb959a89a9220
+	// (should land in TS 1.5)
+	var result = source.replace(/,\((.* => {)/g,', ($1');
+	return result;
+}
+
 findTypeScriptFiles('.').then(function(files) {
 	console.log('found %d files to process', files.length);
 	typescript_formatter.processFiles(files, {
-		replace: true,
+		replace: false,
 		tsfmt: true,
 		editorconfig: false,
 		tslint: false,
-		verbose: true
+		verbose: false,
+		dryRun: true // prevent echoing of output to console
 	}).then(function(results) {
+		Object.keys(results).forEach(function(fileName) {
+			var result = results[fileName];
+			var fixedUpResult = fixupResult(result.dest);
+			if (result.src !== fixedUpResult) {
+				console.log('Updated', fileName);
+				fs.writeFileSync(fileName, fixedUpResult);
+			}
+		});
 		console.log('Formatting complete');
 	}).catch(function(err) {
 		console.error('Processing failed: %s', err.toString());
